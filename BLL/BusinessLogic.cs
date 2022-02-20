ad823b3a344f9abb0b485d34ba8cb94efa916094
REYES, Jean Marc <42404907+jm18reyes@users.noreply.github.com>
2022-02-21 00:23:02 +0800

    Add files via upload

---
+++ BLL/BusinessLogic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using DAL;
+
+namespace BLL
+{
+    public class BusinessLogic
+    {
+       
+        public string ReturnName(string user, string pass)
+        {
+            string FullName;
+            DataAccess Name = new DataAccess();
+            FullName = Name.SearchUsers(user, pass);
+            return FullName;
+        }
+        public bool AddingValue(string user, string pass, string FullName)
+        {
+            bool validity;
+            DataAccess CheckValid = new DataAccess();
+            validity = CheckValid.AddAccount(user, pass, FullName);
+            return validity;
+        }
+
+             
+    }
+}
